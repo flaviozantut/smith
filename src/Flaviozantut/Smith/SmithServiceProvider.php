33fb4ee8dd22a4f488d30925ef5c02a3ced36172
flaviozantut <flaviozantut@gmail.com>
2017-04-13 20:48:26 +0000

    Apply fixes from StyleCI

--- src/Flaviozantut/Smith/SmithServiceProvider.php
+++ src/Flaviozantut/Smith/SmithServiceProvider.php
@@ -1,50 +1,50 @@
-<?php namespace Flaviozantut\Smith;
+<?php
+
+namespace Flaviozantut\Smith;
 
 use Illuminate\Support\ServiceProvider;
 
-class SmithServiceProvider extends ServiceProvider {
+class SmithServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('flaviozantut/smith');
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('flaviozantut/smith');
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['command.smith.smith'] = $this->app->share(function ($app) {
+            return new SmithCommand($app);
+        });
+        $this->commands(
+            'command.smith.smith'
+        );
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['command.smith.smith'] = $this->app->share(function($app)
-		{
-			return new SmithCommand($app);
-		});
-		$this->commands(
-			'command.smith.smith'
-		);
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 }
--- src/commands/SmithCommand.php
+++ src/commands/SmithCommand.php
@@ -1,66 +1,65 @@
-<?php namespace Flaviozantut\Smith;
+<?php
+
+namespace Flaviozantut\Smith;
 
 use Illuminate\Console\Command;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 
-class SmithCommand extends Command {
+class SmithCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'smith';
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'smith';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Boris for laravel';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Boris for laravel';
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $this->info('for more information visit https://github.com/d11wtq/boris');
+        $this->info('press CTRL-D to exit');
+        $boris = new \Boris\Boris('smith> ');
+        $boris->start();
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return void
-	 */
-	public function fire()
-	{
-		$this->info('for more information visit https://github.com/d11wtq/boris');
-		$this->info('press CTRL-D to exit');
-		$boris = new \Boris\Boris('smith> ');
-		$boris->start();
-	}
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [];
+    }
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return array();
-	}
-
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return array();
-	}
-
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [];
+    }
 }
